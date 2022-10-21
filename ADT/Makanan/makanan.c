baa1208d444d9a1a97eed0d202a042a8f8b63f71
Fatih20 <fnri39@protonmail.com>
2022-10-21 20:10:44 +0700

    Fungsi pembanding waktu sampai dan basi. Tambahkan Print makanan.

--- ADT/Makanan/makanan.c
+++ ADT/Makanan/makanan.c
@@ -1,5 +1,7 @@
 #include "makanan.h"
 #include <string.h>
+#include <stdio.h>
+#include "../boolean.h"
 
 void buatMakanan(Makanan *makanan, char *namaMakanan, int waktuBasi, int waktuSampai, int idBaru)
 {
@@ -13,5 +15,80 @@ void buatMakanan(Makanan *makanan, char *namaMakanan, int waktuBasi, int waktuSa
 
     basiDalam(*makanan) = waktuBasi;
     sampaiDalam(*makanan) = waktuSampai;
-    IDDari(*makanan) = idBaru;
+    idDari(*makanan) = idBaru;
 }
+
+void cetakMakanan(Makanan makanan)
+{
+    printf("Nama makanan : ");
+    for (int i = 0; i < strlen(namaMakanan(makanan)); i++)
+    {
+        printf("%c", namaMakanan(makanan)[i]);
+    }
+    printf("\n");
+    printf("Makanan memiliki ID : %d\n", idDari(makanan));
+    printf("Basi dalam : %d menit\n", basiDalam(makanan));
+    printf("Sampai dalam : %d menit\n", sampaiDalam(makanan));
+}
+
+boolean namaMakananSama(Makanan m1, Makanan m2)
+{
+    boolean samaSekarang = strlen(namaMakanan(m1)) == strlen(namaMakanan(m2));
+    int stringLength = strlen(namaMakanan(m1));
+    int i = 0;
+    while (samaSekarang && i < stringLength)
+    {
+        samaSekarang = namaMakanan(m1)[i] == namaMakanan(m2)[i];
+        if (samaSekarang)
+        {
+            i++;
+        }
+    }
+
+    return samaSekarang;
+};
+
+boolean tipeMakananSama(Makanan m1, Makanan m2)
+{
+    return idDari(m1) % 1000 == idDari(m2) % 1000;
+};
+
+boolean basiBersamaan(Makanan m1, Makanan m2)
+{
+    return basiDalam(m1) == basiDalam(m2);
+};
+
+boolean basiDuluan(Makanan m1, Makanan m2)
+{
+    return basiDalam(m1) < basiDalam(m2);
+};
+
+boolean basiDuluanAtauSama(Makanan m1, Makanan m2)
+{
+    return basiDuluan(m1, m2) || basiBersamaan(m1, m2);
+};
+
+boolean basiSetelahAtauSama(Makanan m1, Makanan m2)
+{
+    return !basiDuluan(m1, m2) || basiBersamaan(m1, m2);
+};
+
+boolean sampaiBersamaan(Makanan m1, Makanan m2)
+{
+    return sampaiDalam(m1) == sampaiDalam(m2);
+};
+
+boolean sampaiDuluan(Makanan m1, Makanan m2)
+{
+    return sampaiDalam(m1) < sampaiDalam(m2);
+};
+
+boolean sampaiDuluanAtauSama(Makanan m1, Makanan m2)
+{
+    return sampaiDuluan(m1, m2) || sampaiBersamaan(m1, m2);
+};
+
+boolean sampaiSetelahAtauSama(Makanan m1, Makanan m2)
+{
+    return !sampaiDuluan(m1, m2) || sampaiBersamaan(m1, m2);
+};
--- ADT/Makanan/makanan.h
+++ ADT/Makanan/makanan.h
@@ -1,3 +1,5 @@
+#include "../boolean.h"
+
 #ifndef makanan_H
 #define makanan_H
 
@@ -22,7 +24,7 @@ typedef struct
 #define namaMakanan(S) (S).nama
 #define basiDalam(S) (S).waktuBasi
 #define sampaiDalam(S) (S).waktuSampai
-#define IDDari(S) (S).id
+#define idDari(S) (S).id
 
 /**
  * @brief Fungsi untuk menginisialisasi makanan
@@ -34,5 +36,140 @@ typedef struct
  * @return Makanan pada address yang diberikan terdefinisi dengan parameter yang diberikan
  */
 void buatMakanan(Makanan *makanan, char *namaMakanan, int waktuBasi, int waktuSampai, makananID idBaru);
+
+/**
+ * @brief Fungsi untuk menginisialisasi makanan
+ * @param makanan makanan yang ingin dicetak
+ *
+ * @return Tercetak nama makanan, id, waktu basi, dan waktu sampai
+ */
+void cetakMakanan(Makanan makanan);
+
+/**
+ * @brief Fungsi untuk membandingkan nama makanan
+ * @param m1 makanan yang namanya ingin dibandingkan
+ * @param m2 makanan yang namanya ingin dibandingkan
+ *
+ * @return boolean apakah nama m1 sama dengan m2
+ */
+boolean namaMakananSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan tipe makanan dari 3 id pertama
+ * @param m1 makanan yang namanya ingin dibandingkan
+ * @param m2 makanan yang namanya ingin dibandingkan
+ *
+ * @return boolean apakah tipe m1 sama dengan m2
+ */
+boolean tipeMakananSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk mengecek apakah waktu basi 2 makanan sama
+ * @param m1 makanan yang ingin dibandingkan
+ * @param m2 makanan yang ingin dibandingkan
+ *
+ * @return boolean apakah m1 basi bersamaan dengan m2
+ */
+boolean basiBersamaan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu basi 2 makanan
+ * @brief Jika sama maka akan return false
+ * @param m1 makanan yang waktu basinya ingin dibandingkan
+ * @param m2 makanan yang waktu basinya ingin dibandingkan
+ *
+ * @return boolean apakah m1 basi duluan dibanding m2
+ */
+boolean basiDuluan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu basi 2 makanan
+ * @param m1 makanan yang waktu basinya ingin dibandingkan
+ * @param m2 makanan yang waktu basinya ingin dibandingkan
+ *
+ * @return boolean apakah m1 basi duluan atau bersamaan dengan m2
+ */
+boolean basiDuluanAtauSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu basi 2 makanan
+ * @param m1 makanan yang waktu basinya ingin dibandingkan
+ * @param m2 makanan yang waktu basinya ingin dibandingkan
+ *
+ * @return boolean apakah m1 basi setelah atau bersamaan dengan m2
+ */
+boolean basiSetelahAtauSama(Makanan m1, Makanan m2);
+/**
+ * @brief Fungsi untuk mengecek apakah waktu sampai 2 makanan sama
+ * @param m1 makanan yang ingin dibandingkan
+ * @param m2 makanan yang ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai bersamaan dengan m2
+ */
+boolean sampaiBersamaan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan. Jika sama maka akan return false
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai duluan dibanding m2
+ */
+boolean sampaiDuluan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai duluan atau bersamaan dengan m2
+ */
+boolean sampaiDuluanAtauSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai setelah atau bersamaan dengan m2
+ */
+boolean sampaiSetelahAtauSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk mengecek apakah waktu sampai 2 makanan sama
+ * @param m1 makanan yang ingin dibandingkan
+ * @param m2 makanan yang ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai bersamaan dengan m2
+ */
+boolean sampaiBersamaan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan
+ * @brief Jika sama maka akan return false
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai duluan dibanding m2
+ */
+boolean sampaiDuluan(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai duluan atau bersamaan dengan m2
+ */
+boolean sampaiDuluanAtauSama(Makanan m1, Makanan m2);
+
+/**
+ * @brief Fungsi untuk membandingkan waktu sampai 2 makanan
+ * @param m1 makanan yang waktu sampainya ingin dibandingkan
+ * @param m2 makanan yang waktu sampainya ingin dibandingkan
+ *
+ * @return boolean apakah m1 sampai setelah atau bersamaan dengan m2
+ */
+boolean sampaiSetelahAtauSama(Makanan m1, Makanan m2);
 
 #endif
