01ea2f01a44f2fd538595ce9e4de5e4f238beecd
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2022-10-22 09:39:03 +0700

    Menambahkan header dan implementasi ADT Matriks (Menunggu ADT Simulator dan teksMachine)

---
+++ ADT/Matriks/matriks.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include "matriks.h"
+
+void createMatrix(int nRows, int nCols, Matriks *m)
+{
+    // KAMUS LOKAL
+    // ALGORITMA
+    ROW_EFF(*m) = nRows;
+    COL_EFF(*m) = nCols;
+}
+
+boolean isMatriksIdxValid(int i, int j)
+{
+    // KAMUS LOKAL
+    // ALGORITMA
+    return ((i >= 0 && i < ROW_CAP) && (j >= 0 && j < COL_CAP));
+}
+
+IdxType getLastIdxRow(Matriks m)
+{
+    // KAMUS LOKAL
+    // ALGORITMA
+    return (ROW_EFF(m) - 1);
+}
+
+IdxType getLastIdxCol(Matriks m)
+{
+    // KAMUS LOKAL
+    // ALGORITMA
+    return (COL_EFF(m) - 1);
+}
+
+boolean isIdxEff(Matriks m, IdxType i, IdxType j)
+{
+    // KAMUS LOKAL
+    // ALGORITMA
+    return (i >= 0 && i <= getLastIdxRow(m)) && (j >= 0 && j <= getLastIdxCol(m));
+}
+
+void copyMatriks(Matriks mIn, Matriks *mOut)
+{
+    // KAMUS LOKAL
+    int i, j;
+    // ALGORITMA
+    createMatrix(ROW_EFF(mIn), COL_EFF(mIn), mOut);
+    for (i = 0; i <= getLastIdxRow(mIn); i++)
+    {
+        for (j = 0; j <= getLastIdxCol(mIn); j++)
+        {
+            ELMT(*mOut, i, j) = ELMT(mIn, i, j);
+        }
+    }
+}
+
+void readMatriks(Matriks *m, int nRow, int nCol)
+{
+    // KAMUS LOKAL
+    int i, j, e;
+    // ALGORITMA
+    createMatrix(nRow, nCol, m); // inisialisasi matriks kosong
+}
+
+void displayMatriks(Matriks m)
+{
+    // KAMUS LOKAL
+    int i, j;
+    // ALGORITMA
+    for (i = 0; i <= getLastIdxRow(m); i++)
+    {
+        for (j = 0; j <= getLastIdxCol(m); j++)
+        {
+            if (j != getLastIdxCol(m))
+            {
+                printf("%d ", ELMT(m, i, j));
+            }
+            else
+            {
+                printf("%d", ELMT(m, i, j));
+            }
+        }
+        printf("\n");
+    }
+}
+
+boolean isCollide(Matriks m, Point p)
+{
+    // KAMUS LOKAL
+    int x, y;
+    // ALGORITMA
+    // ditambah 1 untuk menyesuaikan Matriks dengan Peta yang sebenarnya (di luar *)
+    x = Absis(p) + 1;
+    y = Ordinat(p) + 1;
+    return (ELMT(m, x, y) == '*' || ELMT(m, x, y) == 'X' || ELMT(m, x, y) == 'T' || ELMT(m, x, y) == 'M' || ELMT(m, x, y) == 'F' || ELMT(m, x, y) == 'C' || ELMT(m, x, y) == 'B');
+}
+
+void moveSimulator(Matriks *m, Simulator *s, Point p)
+{
+    // KAMUS LOKAL
+    int x1, y1, x2, y2;
+    // ALGORITMA
+
+    // Setup
+    x1 = Absis(*s.lokasi);
+    y1 = Ordinat(*s.lokasi);
+    x2 = Absis(p) + 1;
+    y2 = Ordinat(p) + 1;
+    ELMT(*m, x2, y2) = 'S'; // Mengisi tanda 'S' pada koordinat p
+    ELMT(*m, x1, y1) = ' '; // Menghapus tanda 'S' pada posisi sebelumnya
+    // Mengubah Simulator.lokasi
+    Absis(*s.lokasi) = x2 - 1;
+    Ordinat(*s.lokasi) = y2 - 1;
+}
--- ADT/Matriks/matriks.h
+++ ADT/Matriks/matriks.h
@@ -1,5 +1,131 @@
-#ifndef B74872F0_2614_4989_82D2_8D6C4819D988
-#define B74872F0_2614_4989_82D2_8D6C4819D988
+#include "../boolean.h"
+#include "../Point/point.h"
+#include "../Simulator/simulator.h"
+
+#ifndef MATRIKS_H
+#define MATRIKS_H
+
+/**
+ * @brief Ukuran Maksimal dari Peta diasumsikan 100 x 100, CAPACITY 102 untuk alokasi batas (*)
+ * 
+ */
+#define ROW_CAP 102
+#define COL_CAP 102
+
+typedef int IdxType; 
+typedef char ElPeta;
 
 
-#endif /* B74872F0_2614_4989_82D2_8D6C4819D988 */
+/**
+ * @brief Matriks yang merepresentasikan peta
+ * @param mem Matriks of Char berisi ElPeta
+ * @param rowEff banyaknya/ukuran baris yg terdefinisi
+ * @param collEff banyaknya/ukuran kolom yg terdefinisi
+ */
+typedef struct
+{
+   ElPeta mem[ROW_CAP][COL_CAP];
+   int rowEff; 
+   int colEff;
+} Matriks;
+/* rowEff >= 12 dan colEff >= 12 */
+/* Indeks matriks yang digunakan: [0..ROW_CAP-1][0..COL_CAP-1] */
+/* Memori matriks yang dipakai selalu di "ujung kiri atas" */
+
+/**
+ * @brief Constructor Matriks
+ * @param nRows: Banyak baris 
+ * @param nCols: Banyak kolom
+ * @param m: Matriks(Peta)
+ * @return Terbentuk Sebuah Matriks kosong berukuran nRows x nCols
+ */
+void createMatriks(int nRows, int nCols, Matriks *m);
+
+#define ROW_EFF(M) (M).rowEff
+#define COL_EFF(M) (M).colEff
+#define ELMT(M, i, j) (M).mem[(i)][(j)]
+
+/**
+ * @brief Mengirimkan true jika i, j adalah index yang valid untuk matriks apa pun
+ * 
+ * @param i: Indeks baris 
+ * @param j: Indeks kolom
+ * @return boolean 
+ */
+boolean isMatriksIdxValid(int i, int j);
+
+/**
+ * @brief Mengirimkan indeks baris terbesar m 
+ * 
+ * @param m: Matriks
+ * @return IdxType 
+ */
+IdxType getLastIdxRow(Matriks m);
+
+/**
+ * @brief Mengirimkan Indeks kolom terbesar m 
+ * 
+ * @param m: Matriks 
+ * @return IdxType 
+ */
+IdxType getLastIdxCol(Matriks m);
+
+/**
+ * @brief Mengirimkan true jika i, j adalah Indeks efektif bagi m
+ * 
+ * @param m: Matriks
+ * @param i: Indeks baris 
+ * @param j: Indeks kolom 
+ * @return boolean 
+ */
+boolean isIdxEff(Matriks m, IdxType i, IdxType j);
+
+
+/**
+ * @brief Melakukan assignment mOut <- mIn
+ * 
+ * @param mIn: Matriks yang akan dicopy 
+ * @param mOut: Matriks hasil copy
+ * @return Matriks mOut terdefinisi dan merupakan salinan dari mIn
+ */
+void copyMatriks(Matriks mIn, Matriks *mOut);
+
+
+/**
+ * @brief Melakukan CreateMatrix(m,nRow,nCol) dan mengisi nilai efektifnya
+ * 
+ * @param m: Matriks 
+ * @param nRow: Banyak baris 
+ * @param nCol: Banyak kolom 
+ * @return m terdefinisi nilai elemen efektifnya, berukuran nRow x nCol
+ */
+void readMatriks(Matriks *m, int nRow, int nCol);
+
+/**
+ * @brief Menulis nilai setiap elemen m ke layar dengan traversal per baris dan per kolom
+ * 
+ * @param m: Matriks(peta)
+ * @return Nilai m(i,j) ditulis ke layar per baris per kolom, masing-masing elemen per baris 
+   dipisahkan sebuah spasi
+ */
+void displayMatriks(Matriks m);
+
+/**
+ * @brief Mengembalikan true jika pada koordinat p terdapat batas(*), tembok(X), lokasi suatu tempat(T,M,F,C,B)
+ * 
+ * @param m: Matriks(peta) 
+ * @param p: Koordinat pada peta yang akan dicek
+ * @return boolean 
+ */
+boolean isCollide(Matriks m, Point p);
+
+/**
+ * @brief Memindahkan posisi simulator menuju koordinat p dan mengubah posisi sebelumnya menjadi blank
+ * 
+ * @param m: Matriks(peta) 
+ * @param s: Simulator
+ * @param p: koordinat tujuan 
+ */
+void moveSimulator(Matriks *m, Simulator *s, Point p);
+
+#endif
