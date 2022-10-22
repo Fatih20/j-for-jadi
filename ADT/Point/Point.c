c9300b3b2825eca2702259efe26fabbeb12312d7
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2022-10-22 12:10:12 +0700

    Menambahkan fungsi-fungsi primitif ADT Point

--- ADT/Point/Point.c
+++ ADT/Point/Point.c
@@ -1,28 +1,195 @@
 #include <stdio.h>
 #include "point.h"
 
-void CreatePoint(Point *P, int X, int Y)
+void CreatePoint(POINT *P, int X, int Y)
 {
     Absis(*P) = X;
     Ordinat(*P) = Y;
 }
 
-void moveWest(Point *P)
+void BacaPOINT(POINT *P)
 {
-    Absis(*P)--;
+    /* KAMUS LOKAL*/
+    int X, Y;
+    /* ALGORITMA */
+    scanf("%f %f", &X, &Y);
+    CreatePoint(P, X, Y);
 }
 
-void moveNorth(Point *P)
+void TulisPOINT(POINT P)
 {
-    Ordinat(*P)++;
+    /* KAMUS LOKAL*/
+    float X, Y;
+    /* ALGORITMA*/
+    X = Absis(P);
+    Y = Ordinat(P);
+    printf("(%.02f,%.02f)", X, Y);
 }
 
-void moveEast(Point *P)
+boolean EQ(POINT P1, POINT P2)
 {
-    Absis(*P)++;
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    return (Absis(P1) == Absis(P2)) && (Ordinat(P1) == Ordinat(P2));
 }
 
-void moveSouth(Point *P)
+boolean NEQ(POINT P1, POINT P2)
 {
-    Ordinat(*P)--;
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    return !EQ(P1, P2);
+}
+
+boolean IsOrigin(POINT P)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    return (Absis(P) == 0 && Ordinat(P) == 0);
+}
+
+boolean IsOnSbX(POINT P)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    return (Ordinat(P) == 0);
+}
+
+boolean IsOnSbY(POINT P)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    return (Absis(P) == 0);
+}
+
+int Kuadran(POINT P)
+{
+    /*KAMUS LOKAL*/
+    int X, Y;
+    /*ALGORITMA*/
+    X = Absis(P);
+    Y = Ordinat(P);
+    if (X > 0 && Y > 0)
+    {
+        return 1;
+    }
+    else if (X < 0 && Y > 0)
+    {
+        return 2;
+    }
+    else if (X < 0 && Y < 0)
+    {
+        return 3;
+    }
+    else if (X > 0 && Y < 0)
+    {
+        return 4;
+    }
+}
+
+POINT NextX(POINT P)
+{
+    /*KAMUS LOKAL*/
+    POINT PC;
+    /*ALGORITMA*/
+    Absis(PC) = Absis(P) + 1;
+    Ordinat(PC) = Ordinat(P);
+    return PC;
+}
+
+POINT NextY(POINT P)
+{
+    /*KAMUS LOKAL*/
+    POINT PC;
+    /*ALGORITMA*/
+    Absis(PC) = Absis(P);
+    Ordinat(PC) = Ordinat(P) + 1;
+    return PC;
+}
+
+POINT PlusDelta(POINT P, int deltaX, int deltaY)
+{
+    /*KAMUS LOKAL*/
+    POINT PC;
+    /*ALGORITMA*/
+    Absis(PC) = Absis(P) + deltaX;
+    Ordinat(PC) = Ordinat(P) + deltaY;
+    return PC;
+}
+
+POINT MirrorOf(POINT P, boolean SbX)
+{
+    /*KAMUS LOKAL*/
+    POINT PC;
+    /*ALGORITMA*/
+    if (SbX)
+    {
+        /*Absis Tetap Ordinat dicerminkan*/
+        Absis(PC) = Absis(P);
+        Ordinat(PC) = (-1) * Ordinat(P);
+    }
+    else
+    {
+        /*Ordinat Tetap Absis dicerminkan*/
+        Absis(PC) = (-1) * Absis(P);
+        Ordinat(PC) = Ordinat(P);
+    }
+    return PC;
+}
+
+float Jarak0(POINT P)
+{
+    /*KAMUS LOKAL*/
+    float X, Y;
+    /*ALGORITMA*/
+    X = Absis(P);
+    Y = Ordinat(P);
+    return sqrt(pow(X, 2) + pow(Y, 2)); /*Rumus Jarak*/
+}
+
+float Panjang(POINT P1, POINT P2)
+{
+    /*KAMUS LOKAL*/
+    int deltaX, deltaY;
+    /*ALGORITMA*/
+    deltaX = Absis(P1) - Absis(P2);
+    deltaY = Ordinat(P1) - Ordinat(P2);
+    return sqrt(pow(deltaX, 2) + pow(deltaY, 2)); /*Rumus Jarak*/
+}
+
+void Geser(POINT *P, int deltaX, int deltaY)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    Absis(*P) += deltaX;
+    Ordinat(*P) += deltaY;
+}
+
+void GeserKeSbX(POINT *P)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    Ordinat(*P) = 0;
+}
+
+void GeserKeSbY(POINT *P)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    Absis(*P) = 0;
+}
+
+void Mirror(POINT *P, boolean SbX)
+{
+    /*KAMUS LOKAL*/
+    /*ALGORITMA*/
+    if (SbX)
+    {
+        /*Absis Tetap Ordinat dicerminkan*/
+        Ordinat(*P) *= (-1);
+    }
+    else
+    {
+        /*Ordinat Tetap Absis dicerminkan*/
+        Absis(*P) *= (-1);
+    }
 }
--- ADT/Point/Point.h
+++ ADT/Point/Point.h
@@ -12,7 +12,7 @@ typedef struct
 {
     int X; /* absis   */
     int Y; /* ordinat */
-} Point;
+} POINT;
 
 /**
  * @brief Notasi Akses: Selektor POINT
@@ -23,36 +23,147 @@ typedef struct
 
 /**
  * @brief Membentuk sebuah POINT dari komponen-komponennya
- * @param P: Pointer to Point
+ * @param P: POINT
  * @param X: int
  * @param Y: int
  * @return Terbentuk Point (X,Y) yang terdefinisi nilainya
  */
-void CreatePoint(Point *P, int X, int Y);
+void CreatePoint(POINT *P, int X, int Y);
 
 /**
- * @brief Menggeser Point ke Barat sejauh 1 satuan (x,y) -> (x-1,y)
- * @param P: Pointer to Point
- * @return Point bergeser ke Barat sejauh 1 satuan
+ * @brief Membaca nilai absis dan ordinat dari keyboard dan membentuk
+   POINT P berdasarkan dari nilai absis dan ordinat tersebut
+ * @param P: POINT
+ * @return P terdefinisi
  */
-void moveWest(Point *P);
+void BacaPOINT(POINT *P);
+
 /**
- * @brief Menggeser Point ke Utara sejauh 1 satuan (x,y) -> (x,y+1)
- * @param P: Pointer to Point
- * @return Point bergeser ke Utara sejauh 1 satuan
+ * @brief Nilai P ditulis ke layar dengan format "(X,Y)"
+ * @param P: POINT
+ * @return P tertulis di layar dengan format "(X,Y)"
  */
-void moveNorth(Point *P);
+void TulisPOINT(POINT P);
+
 /**
- * @brief Menggeser Point ke Timur sejauh 1 satuan (x,y) -> (x+1,y)
- * @param P: Pointer to Point
- * @return Point bergeser ke Timur sejauh 1 satuan
+ * @brief Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama
+ * @param P1: POINT
+ * @param P2: POINT
+ * @return boolean
  */
-void moveEast(Point *P);
+boolean EQ(POINT P1, POINT P2);
+
 /**
- * @brief Menggeser Point ke Selatan sejauh 1 satuan (x,y) -> (x,y-1)
- * @param P: Pointer to Point
- * @return Point bergeser ke Selatan sejauh 1 satuan
+ * @brief Mengirimkan true jika P1 tidak sama dengan P2
+ * @param P1: POINT
+ * @param P2: POINT
+ * @return boolean
  */
-void moveSouth(Point *P);
+boolean NEQ(POINT P1, POINT P2);
+
+/**
+ * @brief Menghasilkan true jika P adalah titik origin
+ * @param P: POINT
+ * @return boolean
+ */
+boolean IsOrigin(POINT P);
+
+/**
+ * @brief Menghasilkan true jika P terletak Pada sumbu X
+ * @param P: POINT
+ * @return boolean
+ */
+boolean IsOnSbX(POINT P);
+
+/**
+ * @brief Menghasilkan true jika P terletak pada sumbu Y
+ * @param P: POINT
+ * @return boolean
+ */
+boolean IsOnSbY(POINT P);
+
+/**
+ * @brief Menghasilkan kuadran dari P: 1, 2, 3, atau 4
+ * @param P: POINT
+ * @return int
+ */
+int Kuadran(POINT P);
+
+/**
+ * @brief Mengirim salinan P dengan absis ditambah satu
+ * @param P: POINT
+ * @return POINT
+ */
+POINT NextX(POINT P);
+
+/**
+ * @brief Mengirim salinan P dengan ordinat ditambah satu
+ * @param P: POINT
+ * @return POINT
+ */
+POINT NextY(POINT P);
+
+/**
+ * @brief Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY
+ * @param P: POINT
+ * @param deltaX: Pergeseran terhadap sumbu-x
+ * @param deltaY: Pergeseran terhadap sumbu-y
+ * @return POINT
+ */
+POINT PlusDelta(POINT P, int deltaX, int deltaY);
+
+/**
+ * @brief Menghasilkan salinan P yang dicerminkan terhadap salah satu sumbu. Jika SbX bernilai true, maka dicerminkan terhadap sumbu X, jika tidak, maka dicerminkan terhadap sumbu Y
+ * @param P: POINT
+ * @param SbX: Apakah dicerminkan terhadap sumbu X atau tidak
+ * @return POINT
+ */
+POINT MirrorOf(POINT P, boolean SbX);
+
+/**
+ * @brief Menghitung jarak P ke (0,0)
+ * @param P: POINT
+ * @return float
+ */
+float Jarak0(POINT P);
+
+/**
+ * @brief Menghitung panjang garis yang dibentuk P1 dan P2
+ *
+ * @param P1: POINT
+ * @param P2: POINT
+ * @return float
+ */
+float Panjang(POINT P1, POINT P2);
+
+/**
+ * @brief Menggeser absis P sebesar deltaX dan ordinatnya sebesar deltaY
+ * @param P: POINT
+ * @param deltaX: Pergeseran terhadap sumbu X
+ * @param deltaY: Pergeseran terhadap sumbu Y
+ * @return P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY
+ */
+void Geser(POINT *P, int deltaX, int deltaY);
+
+/**
+ * @brief Menggeser P ke sumbu X dengan Absis yang sama
+ * @param P: POINT
+ * @return P berada pada sumbu X dengan absis sama dengan absis semula
+ */
+void GeserKeSbX(POINT *P);
+/**
+ * @brief Menggeser P ke sumbu Y dengan Ordinat yang sama
+ * @param P: POINT
+ * @return P berada pada sumbu Y dengan Ordinat sama dengan Ordinat semula
+ */
+void GeserKeSbY(POINT *P);
+
+/**
+ * @brief Mencerminkan P terhadap sumbu X jika SbX true, mencerminkan terhadap sumbu Y jika SbX false
+ * @param P: POINT
+ * @param SbX: boolean
+ * @return P dicerminkan tergantung nilai SbX atau SbY
+ */
+void Mirror(POINT *P, boolean SbX);
 
 #endif
