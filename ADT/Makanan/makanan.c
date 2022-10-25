1e25a642d6d5a4d7d8bdc7f2dfc88ab8a0a2d807
Fatih20 <fnri39@protonmail.com>
2022-10-25 22:21:08 +0700

    Add print newline at the end of printing food.

--- ADT/Makanan/makanan.c
+++ ADT/Makanan/makanan.c
@@ -30,6 +30,7 @@ void cetakMakanan(Makanan makanan)
     tulisWaktu(sampaiDalam(makanan));
     printf("\n");
     displayAksiLokasi(aksiLokasi(makanan));
+    printf("\n");
 }
 
 Teks idMakanan(Makanan makanan)
