9bb6a357dfda6b049f4cd0b170df764af0b0f39e
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2022-10-25 22:58:38 +0700

    Menyesuaikan driver dengan state

--- ADT/Stack/runStack.bat
+++ ADT/Stack/runStack.bat
@@ -1,1 +1,1 @@
-gcc stackDriver.c stack.c ../State/state.c ../Point/Point.c ../FoodQueue/foodQueue.c ../ListDinMakanan/ldinmakanan.c ../Makanan/makanan.c ../Teks/teks.c ../ListDinKar/ldinkar.c -o stackDriver
+gcc stackDriver.c stack.c ../State/state.c ../Point/Point.c ../FoodQueue/foodQueue.c ../ListDinMakanan/ldinmakanan.c ../Makanan/makanan.c ../Teks/teks.c ../ListDinKar/ldinkar.c ../Waktu/waktu.c ../AksiLokasi/aksiLokasi.c -o stackDriver
