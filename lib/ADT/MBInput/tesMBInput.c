963f95aef105b6ff3ab7462d265d03278c5b01af
Fatih20 <fnri39@protonmail.com>
2022-11-01 12:09:48 +0700

    Better test for MBInput.

--- lib/ADT/MBInput/tesMBInput.c
+++ lib/ADT/MBInput/tesMBInput.c
@@ -16,11 +16,5 @@ int main(int argc, char const *argv[])
     startMBInput();
     printLDinTeks(currentRowI);
 
-    // do
-    // {
-    //     printLDinTeks(currentRowI);
-    //     advMBInput();
-    // } while (!endMBI);
-
     return 0;
 }
